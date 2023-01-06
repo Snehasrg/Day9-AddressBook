743201eb707ecc939a8dddaee0df99592af1c05a
Snehasrg <sghongade642@gmail.com>
2023-01-06 18:22:22 +0530

    UserDetails

---
+++ Contactinfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contactinfo
+    {
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+
+        public string Address { get; set; }
+        public string State { get; set; }
+
+        public string City { get; set; }
+
+        public int Zip { get; set; }
+        public long Phone_number { get; set; }
+
+        public string Email { get; set; }
+    }
+}
