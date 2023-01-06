63afbc49abb168b11b3e39d9a78f346135ee5cfa
Snehasrg <sghongade642@gmail.com>
2023-01-06 17:34:59 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@
         public static void Main(String[] args)
         {
             //Console.WriteLine(" Welcome to Address Book Program");
-            Contact obj = new Contact("Ram","Sharma","JubliHills,pune","Pune","Maharashtra","90200","92xxxxxxxx","ramsharma@gmail.com" );
-           // Contact obj1 = new Contact();
+           // Contact obj = new Contact("Ram","Sharma","JubliHills,pune","Pune","Maharashtra","90200","92xxxxxxxx","ramsharma@gmail.com" );
+           // obj.Add();
+            AddContact obj = new AddContact();
             obj.Add();
  
         }
